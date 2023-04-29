892cb599d8a7998d1fd505a50b8eef7409c4a9ed
Brian Lauber <blauber@arclia.com>
2023-04-29 14:45:19 -0400

    Partially extracted entities.invader ...
    ... however, the explosion logic should really be handled within
    this module as well. Still need to do some further untangling to
    achieve that.

---
+++ src/arclia/kerblaxion/entities/invader.py
@@ -0,0 +1,76 @@
+
+import pygame
+
+from arclia.pubsub import Publisher
+from arclia.happygame.math import Vector2Coercible
+
+from ..assets import get_sound, get_surface
+
+from .core import UpdateContext
+
+
+class Explosion(pygame.sprite.Sprite):
+  def __init__(self,
+    position: Vector2Coercible,
+  ):
+    super().__init__()
+    self.position = pygame.Vector2(position)
+
+    self.explode_index = 0
+    self.explosions = [
+      get_surface(f"explode0{i+1}.png")
+      for i in range(4)
+    ]
+
+    self.started_at = pygame.time.get_ticks()
+
+
+  @property
+  def image(self):
+    return self.explosions[self.explode_index]
+
+  @property
+  def rect(self):
+    return self.image.get_rect(
+      center = self.position,
+    )
+
+  def update(self, ctx: UpdateContext):
+    # Update the explosion animation every 32 milliseconds
+    self.explode_index = (ctx.t_ms - self.started_at) >> 5
+
+    if self.explode_index >= len(self.explosions):
+      self.kill()
+
+
+class Enemy(pygame.sprite.Sprite):
+  def __init__(self,
+    position: Vector2Coercible,
+  ):
+    super().__init__()
+    self.position = pygame.Vector2(position)
+
+    self.image = get_surface("enemy01.png")
+
+    self.direction = +1
+
+    [self.on_destroyed, self._emit_destroyed] = Publisher[Enemy].methods()
+
+  @property
+  def rect(self):
+    return self.image.get_rect(
+      center = self.position,
+    )
+
+  def destroy(self):
+    self._emit_destroyed(self)
+    self.kill()
+
+  def update(self, ctx: UpdateContext):
+    self.position.x += (self.direction)
+
+    r = self.rect
+    if r.right >= 320 or r.left <= 0:
+      self.direction = -self.direction
+      self.position.x += (self.direction)
+      self.position.y += 8
--- src/arclia/kerblaxion/hero.py
+++ src/arclia/kerblaxion/hero.py
@@ -11,74 +11,12 @@ from .ui.score import Scoreboard
 from .game import Scene
 
 from .entities.core import UpdateContext
+from .entities.invader import Enemy, Explosion
 from .entities.hero import Hero
 from .entities.star import Star
 
 
-class Explosion(pygame.sprite.Sprite):
-  def __init__(self,
-    position: Vector2Coercible,
-  ):
-    super().__init__()
-    self.position = pygame.Vector2(position)
 
-    self.explode_index = 0
-    self.explosions = [
-      get_surface(f"explode0{i+1}.png")
-      for i in range(4)
-    ]
-
-    self.started_at = pygame.time.get_ticks()
-
-  @property
-  def image(self):
-    return self.explosions[self.explode_index]
-
-  @property
-  def rect(self):
-    return self.image.get_rect(
-      center = self.position,
-    )
-
-  def update(self, ctx: UpdateContext):
-    # Update the explosion animation every 32 milliseconds
-    self.explode_index = (ctx.t_ms - self.started_at) >> 5
-
-    if self.explode_index >= len(self.explosions):
-      self.kill()
-
-
-class Enemy(pygame.sprite.Sprite):
-  def __init__(self,
-    position: Vector2Coercible,
-  ):
-    super().__init__()
-    self.position = pygame.Vector2(position)
-
-    self.image = get_surface("enemy01.png")
-
-    self.direction = +1
-
-    [self.on_destroyed, self._emit_destroyed] = Publisher[Enemy].methods()
-
-  @property
-  def rect(self):
-    return self.image.get_rect(
-      center = self.position,
-    )
-
-  def destroy(self):
-    self._emit_destroyed(self)
-    self.kill()
-
-  def update(self, ctx: UpdateContext):
-    self.position.x += (self.direction)
-
-    r = self.rect
-    if r.right >= 320 or r.left <= 0:
-      self.direction = -self.direction
-      self.position.x += (self.direction)
-      self.position.y += 8
 
 class GameScene(Scene):
   def __init__(self):
@@ -125,6 +63,8 @@ def prepare():
 
   def handle_enemy_destroyed(enemy: Enemy):
     scene.scoreboard.score += 250
+
+    # FIXME: These details should be handled by the enemy
     explosion = Explosion(position = enemy.position)
     scene.visible_sprites.add(explosion)
     explode_sfx.play()
